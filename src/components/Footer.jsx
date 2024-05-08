81cb1aa862875beafcc928f8655da15f8724bc11
Thu Ta Naing <thutanaing.personal@gmail.com>
2024-05-08 20:19:04 +0630

    Small Fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,15 @@ function Footer() {
         <div className="w-full h-24 bg-colors-background text-center overflow-hidden">
             <div className='flex flex-col lg:flex-row w-[90%] h-14 border-t-2 border-colors-secondary mx-auto'>
                 <p className='mt-auto p-2 text-sm text-slate-600'>
-                    &copy; Coloris by 
+                    &copy; Calor by 
                     <b className='text-[17px] text-black'> Yion </b>
                     (smol brain developer)
                 </p>
                 <div className='flex h-10 w-[13%] ml-20 lg:ml-auto mt-auto mr-1 p-3 space-x-5'>
-                    <a href="#" className='contact-btn'><AtSign /></a>
-                    <a href="#" className='contact-btn'><Linkedin /></a>
-                    <a href="#" className='contact-btn'><Github /></a>
-                    <a href="#" className='contact-btn'><Facebook /></a>
+                    <a href="mailto:thutanaing.personal@gmail.com" className='contact-btn'><AtSign /></a>
+                    <a href="www.linkedin.com/in/thuta-naing-83b5222b0" className='contact-btn'><Linkedin /></a>
+                    <a href="https://github.com/yion69" className='contact-btn'><Github /></a>
+                    <a href="https://www.facebook.com/thuta.naing.353?mibextid=LQQJ4d" className='contact-btn'><Facebook /></a>
                 </div>
             </div>
         </div>
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -5,7 +5,7 @@ function NavigationBar () {
     
     return (
         <div className='w-100 h-24 flex flex-col place-content-center place-items-center bg-colors-background text-colors-text'>
-            <h3 className='mt-7 text-3xl font-bold drop-shadow-xl'>Color Picker <span className='text-sm'>V2</span></h3>
+            <h3 className='mt-7 ml-4 text-3xl font-bold drop-shadow-xl'>Calor<span className='text-xs'>color-picker</span></h3>
             <ul className='flex mb-2 pt-2 space-x-10 drop-shadow-xl text-md'>
                 <li className='transition 200ms hover:scale-110 hover:font-bold'>
                     <Link to="/">Home</Link>
